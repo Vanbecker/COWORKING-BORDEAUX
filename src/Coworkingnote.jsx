59e9807e875533f9daf36d907d9325b446879fa1
Rémi Le Gall <126484877+Vanbecker@users.noreply.github.com>
2023-07-10 14:39:41 +0200

    fonctionne

--- src/Coworkingnote.jsx
+++ src/Coworkingnote.jsx
@@ -1,14 +1,11 @@
-
-
-
+import Coworkingglobal from "./Coworkingglobal "
 const CoworkingNote = ({ coworkings }) => {
     const filteredCoworkings = coworkings.filter(coworking => coworking.rating >= 4);
 
     return (
         <div>
-            <h2>Coworkings avec une note élevée (4/5 et plus)</h2>
             {filteredCoworkings.map((coworkingLoop) => (
-                <Coworkingglobal coworkingProps={coworkingLoop} />
+                <Coworkingglobal coworking={coworkingLoop} />
             ))}
         </div>
 
